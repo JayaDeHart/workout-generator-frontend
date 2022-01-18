9f793d7700ab48b16a1180841c5d913ccad918fc
Jaya DeHart <dehartj97@gmail.com>
2022-01-18 15:29:50 -0800

    algorithms

--- src/algorithms/algorithms.js
+++ src/algorithms/algorithms.js
@@ -18,18 +18,19 @@ function newWeight(load, reps, sets) {
   return load / (reps * sets);
 }
 
-const reps = {
+//[reps,sets] for each week
+const scheme = {
   hypertrophy: {
-    one: 12,
-    two: 12,
-    three: 10,
-    four: 8,
+    one: [12, 3],
+    two: [12, 3],
+    three: [10, 3],
+    four: [10, 3],
   },
   strength: {
-    one: 6,
-    two: 6,
-    three: 4,
-    four: 4,
+    one: [6, 4],
+    two: [6, 4],
+    three: [4, 4],
+    four: [4, 4],
   },
 };
 
@@ -37,6 +38,6 @@ module.exports.OrmToReps = OrmToReps;
 module.exports.RepsORMtoWeight = RepsORMtoWeight;
 module.exports.volumeLoad = volumeLoad;
 module.exports.newWeight = newWeight;
-module.exports.reps = reps;
+module.exports.scheme = scheme;
 
 //at some point we need to subtract 5% from initial one rep maximum. Best to do it early and only in one place
--- src/algorithms/generator.js
+++ src/algorithms/generator.js
@@ -3,52 +3,61 @@ const {
   RepsORMtoWeight,
   volumeLoad,
   newWeight,
-  reps,
+  scheme,
 } = require('./algorithms');
 
-let hypertrophy = {};
-let strength = {};
-
+//sample data for testing
 const orms = {
   bench: 10,
   squat: 11,
   rows: 12,
   deadlifts: 13,
 };
-function mesoCycle(cycle, orms) {
-  //generate initial load, use it to calculate 5% increase
-  const [one, load] = weekOne(orms, 3, 12);
-  if (cycle === 'hypertrophy')
-    return {
-      weekOne: one,
-      weekTwo: weekGenerator(load, 1),
-    };
-}
 
 function weekOne(orms, sets, reps) {
   const output = [];
-  const scheme = [];
-  //this is wrong! we need 4x initial loads and 4x initial weights
-  //maybe better to have sub "exercise functions" called by the week functions
-  const weight = RepsORMtoWeight(reps, 0.95 * orm);
-  const load = volumeLoad(weight, sets, reps);
   for (const [key, value] of Object.entries(orms)) {
-    scheme.push({
+    const weight = RepsORMtoWeight(reps, 0.95 * value);
+    output.push({
       exercise: key,
+      weight: weight,
       sets: sets,
       reps: reps,
     });
   }
-  output.push(scheme);
-  //this is where we get initial load. Increase by 5% each time
-  output.push(load);
   return output;
 }
 
-function weekGenerator(load, week, sets, reps) {
+function macroCycle() {
+  //use the reps object here to generate mesocycles ie. reps.hypertrophy and reps.strength
+  return {
+    hypertrophy: mesoCycle(reps.hypertrophy),
+    strength: mesoCycle(reps.strength),
+  };
+}
+
+function mesoCycle(reps) {
+  //use the reps object here to assign reps to each week
+}
+
+function microCycle(prevWeek) {
   const output = [];
-  const newLoad = load + load * 0.05 * week;
-  const newWeight = newLoad / (sets, reps);
+  prevWeek.forEach((obj) => {
+    //generate load
+    //pass exercise name and load to eGen
+    //pass
+  });
+}
+
+function exerciseGen(exercise, load, sets, reps) {
+  //exercise and load
+  const weight = newWeight(load, reps, sets);
+  return {
+    exercise,
+    weight,
+    sets,
+    reps,
+  };
 }
 
 const hypertrophy = {
